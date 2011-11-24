ad1e94ba97610ae1be688b0106b3c73e9c413cd0
Elliott O'Hara <elliott.ohara@gmail.com>
2011-11-24 08:55:06 -0600

    created UserManager and tests

---
+++ AuthenticationService/Tests/AuthenticationService.Tests/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Amazon.SimpleDB;
+using NUnit.Framework;
+
+namespace AuthenticationService.Tests
+{
+    [TestFixture]
+    public class authentication_tests
+    {
+        private string _key;
+        private string _secret;
+        private AmazonSimpleDB _client;
+        private UserManager _manager;
+
+        [SetUp]
+        public void setup()
+        {
+            var streamReader = new StreamReader(@"C:\SpecialSuperSecret\elliott.ohara@gmail.com.txt");
+            
+            var dictionary = new Dictionary<string, string>();
+            while (!streamReader.EndOfStream)
+            {
+                var line = streamReader.ReadLine();
+                var keyValue = line.Split(':');
+                dictionary.Add(keyValue[0], keyValue[1]);
+            }
+            _key = dictionary["UserName"];
+            _secret = dictionary["Password"];
+            _client = Amazon.AWSClientFactory.CreateAmazonSimpleDBClient(_key, _secret);
+            _manager = new UserManager(new AuthenticationServiceConfig() {StoreName = "TEST_AUTHENTICATION"}, _client);
+
+        }
+        [Test]
+        public void create_user()
+        {
+            _manager.CreateUser(new CreateUserRequest{UserName = "Elliott",Password = "test"});
+        }
+        [Test]
+        public void authenticate_true()
+        {
+            Assert.True(_manager.Authenticate(new AutheticateRequest {UserName = "Elliott", Password = "test"}));
+        }
+        [Test]
+        public void authenticate_false()
+        {
+            Assert.False(_manager.Authenticate(new AutheticateRequest{UserName = "Elliott",Password = "nope"}));
+        }
+        [Test]
+        public void add_to_group()
+        {
+            _manager.AddToGroup(new AddToGroupRequest{UserName = "Elliott",Group = "ops"});
+        }
+        [Test]
+        public void get_groups()
+        {
+            var respons = _manager.GetGroups(new GetGroupsRequest {UserName = "Elliott"});
+            Assert.Contains("Admin",respons.Groups.ToList());
+            Assert.Contains("ops", respons.Groups.ToList());
+        }
+
+    }
+}
---
+++ src/AuthenticationService/AuthenticationService/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+
+namespace AuthenticationService
+{
+    public class AuthenticationServiceConfig
+    {
+        public string StoreName { get; set; }
+    }
+    public class UserManager
+    {
+        private readonly AuthenticationServiceConfig _config;
+        private readonly AmazonSimpleDB _client;
+
+        public UserManager(AuthenticationServiceConfig config, AmazonSimpleDB client)
+        {
+            _config = config;
+            _client = client;
+            EnsureDomain();
+        }
+
+        private void EnsureDomain()
+        {
+            _client.CreateDomain(new CreateDomainRequest().WithDomainName(_config.StoreName));
+        }
+
+        /// <summary>
+        /// Creates a user. 
+        /// </summary>
+        /// <param name="request"></param>
+        public void CreateUser(CreateUserRequest request)
+        {
+            var putRequest = new PutAttributesRequest().WithDomainName(_config.StoreName)
+                .WithItemName(request.UserName)
+                .WithAttribute(new ReplaceableAttribute().WithName("Password").WithValue(request.Password))
+                ;
+            _client.PutAttributes(putRequest);
+        }
+        public bool Authenticate(AutheticateRequest request)
+        {
+            var getAttribute =
+                new GetAttributesRequest().WithDomainName(_config.StoreName).WithItemName(request.UserName);
+            var result = _client.GetAttributes(getAttribute).GetAttributesResult;
+            if (result == null) return false;
+            return result.Attribute.Any(a => a.Name == "Password" && a.Value == request.Password);
+        }
+        public void AddToGroup(AddToGroupRequest request)
+        {
+            var putAttribute = new PutAttributesRequest().WithDomainName(_config.StoreName).WithItemName(
+                request.UserName)
+                .WithAttribute(new ReplaceableAttribute().WithName("Groups").WithValue(request.Group));
+            _client.PutAttributes(putAttribute);
+        }
+        public GetGroupsResponse GetGroups(GetGroupsRequest request)
+        {
+            var getAttributesRequest =
+                new GetAttributesRequest().WithDomainName(_config.StoreName).WithItemName(request.UserName);
+            var results = _client.GetAttributes(getAttributesRequest).GetAttributesResult;
+            var groups = new List<String>();
+            foreach (var attribute in results.Attribute.Where(a => a.Name == "Groups"))
+            {
+                groups.Add(attribute.Value);
+            }
+            return new GetGroupsResponse {Groups = groups, UserName = request.UserName};
+        }
+
+                 
+    }
+    public class GetGroupsRequest
+    {
+        public string UserName { get; set; }
+    }
+    public class GetGroupsResponse
+    {
+        public string UserName { get; set; }
+        public IEnumerable<string> Groups { get; set; } 
+    }
+    public class CreateUserRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class AutheticateRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class AddToGroupRequest
+    {
+        public string UserName { get; set; }
+        public string Group { get; set; }
+    }
+    
+}
